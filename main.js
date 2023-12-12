b3c93395942e20a77df7359ccb2ccd27a0aea63d
unknown <radioelpalaciovallenato@gmail.com>
2023-12-12 07:33:54 -0500

    luta

---
+++ main.js
@@ -0,0 +1,13 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const slider = document.querySelector(".slider");
+
+prev .addEventListener ("click", () => (
+    slider.scrollLeft -= 300
+))
+    
+next .addEventListener ("click", () => (
+    slider.scrollLeft += 300
+))
+
+/*____________ Giratorias vallenatas _______*/
